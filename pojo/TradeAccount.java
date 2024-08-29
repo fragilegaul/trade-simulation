2c40f21d3c980f5cfcc9719e0f48e11119e061cb
ann <hedion@proton.me>
2024-08-29 19:38:30 +0200

    Initial commit

---
+++ pojo/TradeAccount.java
@@ -0,0 +1,18 @@
+package pojo;
+
+public abstract class TradeAccount {
+    private String id;
+
+    public TradeAccount(String id) {
+        this.id = id;
+    }
+
+    public abstract TradeAccount clone();
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getId() {
+        return this.id;
+    }
+}
